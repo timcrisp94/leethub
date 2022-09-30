4aa355de7b37f35a5391cd4c1f092fe27a31186f
Tim Crisp <95889986+timcrisp94@users.noreply.github.com>
2022-09-30 08:59:42 -0500

    Time: 72 ms (88.06%), Space: 43.5 MB (6.13%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -1,18 +1,13 @@
-class Solution(object):
-  def twoSum(self, nums, target):
-    """
-    :type nums: List[int]
-    :type target: int
-    :rtype: List[int]
-    """
-    l, r = 0, len(nums) - 1
-
-    while l < r:
-      sum = nums[l] + nums[r]
-      
-      if sum > target:
-        r -= 1
-      elif sum < target:
-        l += 1
-      else:
-        return [l+1, r+1]
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let p1=0, p2=numbers.length;
+    while(p1<p2){
+        if(numbers[p1]+numbers[p2]==target) return [p1+1,p2+1];
+        else if(numbers[p1]+numbers[p2]<target) p1++;
+        else p2--;
+    }
+};
