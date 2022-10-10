2e8c37916b75ec4670d91b23c7afc277a8a48024
Tim Crisp <95889986+timcrisp94@users.noreply.github.com>
2022-10-10 09:33:43 -0500

    Time: 85 ms (67.18%), Space: 41.8 MB (80.20%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -3,24 +3,6 @@
  * @return {number}
  */
 var findMin = function(nums) {
-  const n = nums.length
-  let left = 0
-  let right = n - 1
-
-  while (left < right) {    
-    const mid = (left + right) >> 1
-    const guess = nums[mid]
-    const [leftNum, rightNum] = [nums[left], nums[right]]
-
-    const isTarget = leftNum < rightNum
-    if (isTarget) return leftNum
-    
-    const greater = leftNum <= guess
-    if (greater) left = mid + 1
-
-    const less = guess < leftNum
-    if (less) right = mid
-  }
-  return nums[left]
-    
+    return Math.min(...nums)
 };
+
