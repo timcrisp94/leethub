301d70a8d8355010b44dc21d417c8e0cd8555e1f
Tim Crisp <95889986+timcrisp94@users.noreply.github.com>
2022-10-12 08:17:46 -0500

    Time: 109 ms (87.92%), Space: 47.6 MB (59.22%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,20 +3,25 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    const n = s.length
-    const map = new Map()
-    let left = 0
-    let longest = 0
-    
-    for (let right = 0; right < n; right++) {
-        let curChar = s[right]
-        let curCharPos = map.get(curChar)
-        
-        while (curCharPos >= left) {
-            left = curCharPos + 1
-        }
-        map.set(curChar, right)
-        longest = Math.max(longest, right - left + 1)
+  var sLen = s.length,
+    maxLen = 0,
+    maxStr = '',
+    tmpStr,
+    posIndex,
+    i;
+
+  for( i = 0 ; i < sLen; i++ ){
+
+    tmpStr = s[i];
+    posIndex = maxStr.indexOf(tmpStr);
+
+    if(posIndex > -1){
+      maxStr = maxStr.substring(posIndex + 1);
     }
-    return longest
+
+    maxStr += tmpStr;
+    maxLen = Math.max(maxLen, maxStr.length);
+  }
+
+  return maxLen;
 };
