bf67504596d5f69a105709f0a887b154efee0d7b
Tim Crisp <95889986+timcrisp94@users.noreply.github.com>
2022-10-13 10:07:15 -0500

    Time: 255 ms (94.52%), Space: 28.8 MB (75.19%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,31 +4,23 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
+       
         if not grid:
-          return 0
-        
-        rows, cols = len(grid), len(grid[0])
-        visited = set()
-        islands = 0
+            return 0
 
-        def bfs(r,c):
-          q = deque()
-          visited.add((r,c))
-          q.append((r,c))
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
+                    count += 1
+        return count
 
-          while q:
-            row,col = q.popleft()
-            directions = [[1,0], [-1,0], [0,1], [0,-1]]
-
-            for dr, dc in directions:
-              r,c = row + dr, col + dc
-              if (r) in range(rows) and (c) in range(cols) and grid[r][c] == '1' and (r,c) not in visited:
-                q.append((r,c))
-                visited.add((r,c))
-
-        for r in range(rows):
-          for c in range(cols):
-            if grid[r][c] == '1' and (r,c) not in visited:
-              bfs(r,c)
-              islands += 1
-        return islands
+    def dfs(self, grid, i, j):
+        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
+            return
+        grid[i][j] = '#'
+        self.dfs(grid, i+1, j)
+        self.dfs(grid, i-1, j)
+        self.dfs(grid, i, j+1)
+        self.dfs(grid, i, j-1)
