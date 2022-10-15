d4f49d40da7e6bf1af18492a9b78d323319614ae
Tim Crisp <95889986+timcrisp94@users.noreply.github.com>
2022-10-15 11:41:39 -0500

    Time: 220 ms (52.23%), Space: 21.2 MB (21.37%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -4,15 +4,6 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-#         n = len(s)
-#         dp = [[0] * n for _ in range(n)]
-        
-#         res = 0
-#         for i in range(n-1,-1,-1):
-#             for j in range(i, n):
-#                 dp[i][j] = s[i] == s[j] and ((j-1+1) < 3 or dp[i+1][j-1])
-#                 res += dp[i][j]
-#         return res
         n = len(s)
         dp = [[0] * n for _ in range(n)]
         
