6503fdb253bcc5a55e3a1306e0049eec7af4f96f
Tim Crisp <95889986+timcrisp94@users.noreply.github.com>
2022-10-18 08:29:28 -0500

    Time: 187 ms (69.82%), Space: 49.8 MB (88.10%) - LeetHub

---
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.js
@@ -0,0 +1,59 @@
+/**
+ * @param {number[][]} heights
+ * @return {number[][]}
+ */
+const pacificAtlantic = function(matrix) {
+  if (matrix.length === 0) return []
+
+  const [m,n] = [matrix.length, matrix[0].length]
+  const pacificVisited = new Set()
+  const atlanticVisited = new Set()
+  const directions = [
+    [1,0],
+    [-1,0],
+    [0,1],
+    [0,-1]
+  ]
+
+  const intersection = (setA, setB) => {
+    let result = new Set()
+    for (let el of setB) {
+      if (setA.has(el)) {
+        result.add(el)
+      }
+    }
+    return result
+  }
+
+  const dfs = (i, j, visited) => {
+    const pair = `${i}-${j}`
+    if (visited.has(pair)) return
+    visited.add(pair)
+
+    for (const direction of directions) {
+      const [nextI, nextJ] = [i + direction[0], j + direction[1]]
+
+      if (
+        0 <= nextI &&
+        nextI < m &&
+        0 <= nextJ &&
+        nextJ < n &&
+        matrix[nextI][nextJ] >= matrix[i][j]
+      ) {
+        dfs(nextI, nextJ, visited)
+      }
+    }
+  }
+
+  for (let row = 0; row < m; row++) {
+    dfs(row, 0, pacificVisited)
+    dfs(row, n - 1, atlanticVisited)
+  }
+
+  for (let col = 0; col < n; col++) {
+    dfs(0, col, pacificVisited)
+    dfs(m - 1, col, atlanticVisited)
+  }
+
+  return [...intersection(pacificVisited, atlanticVisited)].map(pair => pair.split('-').map(Number))
+}
