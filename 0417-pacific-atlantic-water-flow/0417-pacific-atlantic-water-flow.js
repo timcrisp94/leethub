b9c3190fdf1544f922b62aaf503b6c577bc0ac7a
Tim Crisp <95889986+timcrisp94@users.noreply.github.com>
2022-10-18 10:00:10 -0500

    Time: 241 ms (39.65%), Space: 51.5 MB (52.86%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.js
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.js
@@ -2,49 +2,48 @@
  * @param {number[][]} heights
  * @return {number[][]}
  */
-const pacificAtlantic = function(matrix) {
-  const [m,n] = [matrix.length, matrix[0].length]
-  if (m === 0) return []
-
-  const pacVisited = new Set()
-  const atlVisited = new Set()
-  const directions = [[1,0],[-1,0],[0,1],[0,-1]]
-
-  const intersection = (setA, setB) => {
-    let result = new Set()
-    for (let el of setB) {
-      if (setA.has(el)) {
-        result.add(el)
-      }
-    }
-    return result
-  }
-  const dfs = (i, j, visited) => {
-    const pair = `${i}-${j}`
-    if (visited.has(pair)) return
-    visited.add(pair)
+const pacificAtlantic = (heights) => {
+    const m = heights.length, n = heights[0].length  
+    const atlanticQueue = []
+    const pacificQueue = []
     
-    for (const direction of directions) {
-      const [nextI, nextJ] = [i + direction[0], j + direction[1]]
-      if (
-        0 <= nextI &&
-        nextI < m &&
-        0 <= nextJ &&
-        nextJ < n &&
-        matrix[nextI][nextJ] >= matrix[i][j]
-      ) {
-        dfs(nextI, nextJ, visited)
-      }
+    for (let x = 0; x < m; x++) {
+        for (let y = 0; y < n; y++) {
+            if (x === m - 1 || y === n - 1) {
+                atlanticQueue.push([x, y])
+            }
+            if (x === 0 || y === 0) {
+                pacificQueue.push([x, y])
+            }
+        }
+    } 
+    const bfs = (queue) => {
+        const isValid = (x, y) => x >= 0 && y >= 0 && x < m && y < n
+        const directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        const visited = new Array(m).fill().map(() => new Array(n).fill(false))
+        while (queue.length) {
+            const [x, y] = queue.shift()
+            visited[x][y] = true
+            for (let dir of directions) {
+                let nx = x + dir[0]
+                let ny = y + dir[1]
+                if (!isValid(nx, ny) || visited[nx][ny]) continue
+                if (heights[nx][ny] >= heights[x][y]) {
+                    queue.push([nx, ny])
+                }
+            }
+        }
+        return visited
     }
-  }
-  for (let row = 0; row < m; row++) {
-    dfs(row, 0, pacVisited)
-    dfs(row, n-1, atlVisited)
-  }
-  for (let col = 0; col < n; col++) {
-    dfs(0, col, pacVisited)
-    dfs(m-1, col, atlVisited)
-  }
-  return [...intersection(atlVisited, pacVisited)].map(pair => pair.split('-').map(Number))
+    const pacific = bfs(atlanticQueue)
+    const atlantic = bfs(pacificQueue)
+    const paths = []
+    for (let x = 0; x < m; x++) {
+        for (let y = 0; y < n; y++) {
+            if (pacific[x][y] && atlantic[x][y]) {
+                paths.push([x, y])
+            }
+        }
+    }
+    return paths
 }
-
