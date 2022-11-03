03916a56db6db90eeed7398099c8872e4e947371
Tim Crisp <95889986+timcrisp94@users.noreply.github.com>
2022-11-03 11:40:06 -0500

    Time: 45 ms (7.89%), Space: 13.4 MB (79.51%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.py
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -1,24 +1,8 @@
-/**
- * @param {string} jewels
- * @param {string} stones
- * @return {number}
- */
-var numJewelsInStones = function(jewels, stones) {
-//     jewels = types of stones that are jewels
-//     stones = stones you have
-    // stones[char] = type of stone
-    const map = {}
-    let count = 0
-    
-    for (let char of jewels){
-        map[char] = (map[char] || 0) + 1
-    }
-    
-    for (let char of stones) {
-        if (map[char]) {
-            count += 1
-        }
-    }
-    return count
-    
-};
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        return sum(map(jewels.count, stones))
