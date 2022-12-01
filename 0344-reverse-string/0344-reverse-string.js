de2098a9dc3ca0294372bfa54cee207caf3dc0d3
Tim Crisp <95889986+timcrisp94@users.noreply.github.com>
2022-12-01 12:58:38 -0600

    Time: 158 ms (52.51%), Space: 51.2 MB (6.51%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -4,8 +4,12 @@
  */
 var reverseString = function(s) {
     const n = s.length
-    for (let [a, b] = [0, n - 1]; a < b; a++, b--) {
-        [s[a], s[b]] = [s[b], s[a]]
+    reverse(0, n - 1)
+    
+    function reverse(i, j) {
+        if (i >= j) return
+        [s[i], s[j]] = [s[j], s[i]]
+        reverse(i + 1, j - 1)
     }
     
 };
